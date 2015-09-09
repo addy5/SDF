f1e78b88497cd2bc8b6fe5826d881468a4e563f7
Addison Chen <addison.k.chen@gmail.com>
2015-09-09 01:10:25 -0700

    added second price chart

--- public/javascripts/map_script.js
+++ public/javascripts/map_script.js
@@ -1,10 +1,50 @@
-//LOAD GOOGLE VISUALIZATION:
+var date = new Date();
+var day = date.getDate();
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var lastMonth = month - 1;
+var lastDay = day;
+var lastYear = year;
+
+var dataToPlot = [];
+
+    //CONVERT DATE INTO STRINGS
+    if(month < 10){
+      month = '0' + month.toString();
+    } else {
+      month = month.toString();
+    }
+
+    if(lastMonth === -1){
+      lastYear = lastYear - 1;
+    } else if(lastMonth < 10){
+      lastMonth = '0' + lastMonth.toString();
+    } else {
+      lastMonth = lastMonth.toString();
+    }
+
+    if(day < 10){
+      day = '0' + day.toString();
+    } else {
+      day = day.toString();
+    }
+
+    if(lastDay === 31) lastDay = 28;
+    if(lastDay < 10){
+      lastDay = '0' + lastDay.toString();
+    } else {
+      lastDay = lastDay.toString();
+    }
+
+    year = year.toString();
+
+
+//LOAD GOOGLE SCATTER VISUALIZATION:
 google.load("visualization", "1", {packages:["corechart"]});
 var buyChart = $('#buyChart');
+var priceChart = $('#priceChart');
 
 function drawChart(a,b,c,d,e) {
-
-  buyChart.fadeIn('slow');
 
   var data = google.visualization.arrayToDataTable(
     [  ['Data', 'Price'],
@@ -27,7 +67,22 @@ function drawChart(a,b,c,d,e) {
   chart.draw(data, options);
 }
 
+//LOAD GOOGLE LINE VISUALIZATION:
+function drawLineChart() {
 
+  var data = google.visualization.arrayToDataTable(dataToPlot);
+
+  var options = {
+    title: '30 Day History',
+    curveType: 'function',
+    legend: { position: 'right' },
+    colors: ['#009688']
+    };
+
+  var chart = new google.visualization.LineChart(document.getElementById('priceChart'));
+
+  chart.draw(data, options);
+}
 
 //CAPITALIIZE PROTOTYPE FOR STRINGS (USED TO CAPITALIZE DAYS):
 String.prototype.capitalize = function(){
@@ -100,21 +155,28 @@ $(document).ready(function() {
     var yearlyHigh = $('.yearlyHigh');
     var yearlyLow = $('.yearlyLow');
 
-    searchButton.on('click', runQuote);
+    searchButton.on('click', function(){
+      runQuote();
+      runPriceHistory();
+    });
 
     query.on('keypress', function (e) {
       var key = e.which || e.keyCode;
-      if (key === 13) return runQuote();
+      if (key === 13){
+         runQuote();
+         return runPriceHistory();
+      }
       return null;
     });
 
-    function runQuote(){
-      var baseURL = 'https://query.yahooapis.com/v1/public/yql?q=';
-      var queryParams = 'select * from yahoo.finance.quote where symbol in ("'+ query.val() +'")';
-      var queryString = encodeURI(baseURL + queryParams);
-      var apiCall = queryString + '&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+// https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22YHOO%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 
-      $.getJSON(apiCall, function(data){
+    function runQuote(){
+
+      var queryParams = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22'+query.val()+'%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+
+      $.getJSON(queryParams, function(data){
           console.log(data.query.results.quote);
           symbol.text(data.query.results.quote.Symbol);
           name.text(data.query.results.quote.Name);
@@ -128,6 +190,24 @@ $(document).ready(function() {
           setTimeout(drawChart(parseFloat(data.query.results.quote.LastTradePriceOnly),parseFloat(data.query.results.quote.DaysHigh),parseFloat(data.query.results.quote.DaysLow),parseFloat(data.query.results.quote.YearHigh),parseFloat(data.query.results.quote.YearLow)),100);
         });
 
+    }
+
+    function runPriceHistory(){
+    var queryParams = 'https://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol in ("'+ query.val() +'") and startDate = "'+lastYear+'-'+lastMonth+'-'+lastDay+'" and endDate = "'+year+'-'+month+'-'+day+'"';
+
+    var queryString = encodeURI(queryParams);
+    var apiCall = queryString + '&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+      dataToPlot = [['Date','Price']];
+
+      $.getJSON(apiCall, function(data){
+        var array = data.query.results.quote;
+        for(var i=array.length-1; i > 0; i-=1){
+            dataToPlot.push( [ array[i].Date, parseFloat(array[i].Close) ] );
+        }
+        console.log(dataToPlot);
+        setTimeout(drawLineChart,1000);
+      });
     }
 
     //BUY FUNCTIONS AND HANDLERS:
@@ -163,12 +243,29 @@ $(document).ready(function() {
     sharesInput.on('input', function(){
       totalSummary.text(sharesInput.val() + ' shares of ' + symbol.text() + ' x ' + price.text());
 
-      if( isNaN(parseInt(sharesInput.val()) ) ){
+      if( isNaN(parseInt(sharesInput.val()) ) || sharesInput.val() === 0 ){
         cancelButton.hide();
         confirmButton.hide();
+        totalSummary.hide();
+        totalPrice.hide();
+        totalTradeFee.hide();
+        totalTrade.hide();
+        totalOddLot.hide();
+        totalOddLotFee.hide();
+        totalFees.hide();
+        total.hide();
+
       } else{
         cancelButton.show();
         confirmButton.show();
+        totalSummary.show();
+        totalPrice.show();
+        totalTradeFee.show();
+        totalTrade.show();
+        totalOddLot.show();
+        totalOddLotFee.show();
+        totalFees.show();
+        total.show();
       }
 
       totalPrice.text( '$' +  (parseInt(sharesInput.val()) * parseFloat(price.text())).toFixed(2) );
@@ -194,11 +291,39 @@ $(document).ready(function() {
     cancelButton.on('click', function(){
       background.fadeOut('slow');
       buyModal.fadeOut('slow');
+      sharesInput.val("");
+      cancelButton.hide();
+      confirmButton.hide();
+      totalSummary.hide();
+      totalPrice.hide();
+      totalTradeFee.hide();
+      totalTrade.hide();
+      totalOddLot.hide();
+      totalOddLotFee.hide();
+      totalFees.hide();
+      total.hide();
     });
 
     var confirmButton = $('.confirmButton');
     confirmButton.on('click', function(){
 
+    });
+
+    var priceToggle = $('.priceToggle');
+    var buyToggle = $('.buyToggle');
+
+    priceToggle.on('click', function(){
+      $(this).css('background-color','rgba(128,128,128,1.0)');
+      buyToggle.css('background-color','rgba(128,128,128,0.5)');
+      buyChart.css('z-index','2');
+      priceChart.css('z-index','3');
+    });
+
+    buyToggle.on('click', function(){
+      $(this).css('background-color','rgba(128,128,128,1.0)');
+      priceToggle.css('background-color','rgba(128,128,128,0.5)');
+      priceChart.css('z-index','2');
+      buyChart.css('z-index','3');
     });
 
 }); //CLOSE JQUERY ON PAGE LOAD FUNCTION
